daeea12df74dc3efd9395617ccddc197b3fa1c43
Subin <109493447+Subin43@users.noreply.github.com>
2024-06-06 12:14:14 +0530

    Update ContactForm.tsx

--- yoga-website-main/src/pages/Contact/ContactForm.tsx
+++ yoga-website-main/src/pages/Contact/ContactForm.tsx
@@ -115,6 +115,7 @@ export default function ContactForm() {
           <input
             type="text"
             id="subject"
+            
             className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-1 focus:ring-green-500"
             value={formData.subject}
             onChange={handleChange}
