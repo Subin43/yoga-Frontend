1855165e1b93c7c2d865169e23d04076561b61f8
Subin <109493447+Subin43@users.noreply.github.com>
2024-06-06 16:18:03 +0530

    Update ContactForm.tsx

--- yoga-website-main/src/pages/Contact/ContactForm.tsx
+++ yoga-website-main/src/pages/Contact/ContactForm.tsx
@@ -132,7 +132,7 @@ export default function ContactForm() {
             required
           ></textarea>
 
-          <Button className="bg-blue-500 hover:bg-blue-700 text-white rounded-md" onClick={handleSend}>
+          <Button className="bg-blue-500 hover:bg-blue-700 text-white rounded-md" onClick={handleSend as any}>
             Send
           </Button>
         </div>
