65f181b06c2f28edfc895cf47d7b83c206f0460a
Subin <109493447+Subin43@users.noreply.github.com>
2024-06-06 16:22:08 +0530

    Update Button.tsx

--- yoga-website-main/src/pages/UI/Button.tsx
+++ yoga-website-main/src/pages/UI/Button.tsx
@@ -1,17 +1,19 @@
 import { motion } from "framer-motion";
+import React from "react";
 
-export default function Button({
-  children,
-  className,
-}: {
+interface ButtonProps {
   children: React.ReactNode;
-  className: string | undefined;
-}) {
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function Button({ children, className, onClick }: ButtonProps) {
   return (
     <motion.button
       className={`${className} md:text-lg md:px-4 md:py-2`}
       whileHover={{ scale: 1.1 }}
       transition={{ type: "spring" }}
+      onClick={onClick}
     >
       {children}
     </motion.button>
