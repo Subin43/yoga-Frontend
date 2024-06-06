e3e4af441e414295aaee6d6ee9a42c476249142f
Subin <109493447+Subin43@users.noreply.github.com>
2024-06-06 17:16:10 +0530

    Update Input.tsx

--- yoga-website-main/src/pages/UI/Input.tsx
+++ yoga-website-main/src/pages/UI/Input.tsx
@@ -1,28 +1,29 @@
-export default function Input({
-  type,
-  placeholder,
-  label,
-  name,
-}: {
+import React, { ChangeEvent } from "react";
+
+interface InputProps {
   type: string;
-  placeholder: string;
-  label: string;
+  placeholder?: string;
   name: string;
-}) {
+  onChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => void;
+  value: string;
+  label?: string;
+}
+
+const Input: React.FC<InputProps> = ({ type, placeholder, name, onChange, value, label }) => {
   return (
-    <div className="flex flex-col gap-2 ">
-      <label htmlFor={label} className="text-sm font-medium text-gray-900">
-        {label}
-      </label>
+    <div className="flex flex-col gap-4">
+      {label && <label htmlFor={name} className="text-sm font-medium">{label}</label>}
       <input
         type={type}
-        placeholder={placeholder}
-        id={label}
-        className="border bg-gray-50 border-gray-300 rounded-xl w-full md:w-[300px] p-2.5 text-sm md:text-base text-gray-900 focus:ring-blue-500 focus:border-blue-600 outline-none"
-        required
+        id={name}
         name={name}
-        autoComplete="new-password"
+        placeholder={placeholder}
+        className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 text-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500"
+        onChange={onChange}
+        value={value}
       />
     </div>
   );
-}
+};
+
+export default Input;
