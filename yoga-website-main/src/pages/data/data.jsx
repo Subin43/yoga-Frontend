e5ba9b6aff8505386124e0971a7abe6f91123594
Subin <109493447+Subin43@users.noreply.github.com>
2024-06-07 19:04:18 +0530

    Create data.jsx

---
+++ yoga-website-main/src/pages/data/data.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+import { Brain, HeartPulse, RefreshCcw, UserCog } from "lucide-react";
+import {
+  FaInstagram,
+  FaFacebookSquare,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+
+export const data = [
+  {
+    id: 1,
+    logo: <Brain />,
+    title: "Asanas",
+    desc: "Asanas are the physical postures practiced in yoga.",
+  },
+  {
+    id: 2,
+    logo: <HeartPulse />,
+    title: "Pranayama",
+    desc: "Pranayama is the practice of controlling one’s breath in yoga.",
+  },
+  {
+    id: 3,
+    logo: <RefreshCcw />,
+    title: "Kriya",
+    desc: "Kriya Yoga involves a series of techniques aimed at purifying the body and mind.",
+  },
+  {
+    id: 4,
+    logo: <UserCog />,
+    title: "Relaxation",
+    desc: "Yoga helps to reduce stress, anxiety, and tension and promotes overall well-being.",
+  },
+];
+
+export const icons = [
+  {
+    id: 1,
+    icon: <FaInstagram />,
+    link: "https://www.instagram.com/ashishu9899?igsh=MWlyOHhzdWR4YWRibg==",
+  },
+  {
+    id: 2,
+    icon: <FaFacebookSquare />,
+    link: "https://www.facebook.com/ashishu9899",
+  },
+  {
+    id: 3,
+    icon: <FaTwitter />,
+    link: "https://x.com/ashishu9899?t=T8IqX1YM5t7_xH5dINpDUg&s=09",
+  },
+  {
+    id: 4,
+    icon: <FaYoutube />,
+    link: "https://youtu.be/9ONp4HAiqlw?si=KNmM8GN-2FyypsLW",
+  },
+];
+
+export const image = [
+  {
+    id: 1,
+    img: "demo/image9.jpeg",
+  },
+  {
+    id: 2,
+    img: "demo/image2.jpeg",
+  },
+  {
+    id: 3,
+    img: "demo/image3.jpeg",
+  },
+  {
+    id: 4,
+    img: "demo/image5.jpeg",
+  },
+  {
+    id: 5,
+    img: "demo/image7.jpeg",
+  },
+  {
+    id: 6,
+    img: "demo/image10.jpeg",
+  },
+  {
+    id: 7,
+    img: "demo/image11.jpeg",
+  },
+];
+
+export const listDatas = [
+  {
+    id: 1,
+    desc: "Yoga improves flexibility, strength, and balance, enhancing overall physical fitness and reducing the risk of injuries.",
+  },
+  {
+    id: 2,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  },
+  {
+    id: 3,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  },
+  {
+    id: 4,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  },
+  {
+    id: 5,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  },
+];
+
+export const ACHIEVEMENT = [
+  {
+    id: 1,
+    img: "demo/image9.jpeg",
+  },
+  {
+    id: 2,
+    img: "demo/image2.jpeg",
+  },
+  {
+    id: 3,
+    img: "demo/image3.jpeg",
+  },
+  {
+    id: 4,
+    img: "demo/image5.jpeg",
+  },
+  {
+    id: 5,
+    img: "demo/image7.jpeg",
+  },
+  {
+    id: 6,
+    img: "demo/image10.jpeg",
+  },
+  {
+    id: 7,
+    img: "demo/image11.jpeg",
+  },
+  {
+    id: 8,
+    img: "",
+  },
+  {
+    id: 9,
+    img: "",
+  },
+];
