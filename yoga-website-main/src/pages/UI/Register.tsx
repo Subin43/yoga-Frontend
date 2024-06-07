abf753116f21dc9107007afd6f3c98cd6f468d18
Subin <109493447+Subin43@users.noreply.github.com>
2024-06-07 18:47:27 +0530

    Update Register.tsx

--- yoga-website-main/src/pages/UI/Register.tsx
+++ yoga-website-main/src/pages/UI/Register.tsx
@@ -1,24 +1,32 @@
 import { useNavigate } from "react-router-dom";
-// import Input from "./Input";
 import axios from "axios";
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 
 export default function Register() {
   const navigate = useNavigate();
   const [error, setError] = useState<boolean | string>("");
+  const [formData, setFormData] = useState({
+    user_name: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    const fd = new FormData(e.currentTarget);
-    const data = Object.fromEntries(fd.entries());
+    const { user_name, password, confirm_password } = formData;
 
     try {
-      if (data.password === data.confirm_password) {
+      if (password === confirm_password) {
         const response = await axios.post("/", {
-          user_name: data.user_name,
-          password: data.password,
-          confirm_password: data.confirm_password,
+          user_name,
+          password,
+          confirm_password,
         });
         setError(false);
         console.log(response.status);
@@ -34,24 +42,51 @@ export default function Register() {
   return (
     <div className="min-h-screen flex justify-center items-center px-4">
       <form className="grid gap-4" onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Enter your email"
-          label="Email"
-          name="user_name"
-        />
-        <input
-          type="password"
-          placeholder="Enter your password"
-          label="Password"
-          name="password"
-        />
-        <input
-          type="password"
-          placeholder="Confirm password"
-          label="Confirm Password"
-          name="confirm_password"
-        />
+        <div className="flex flex-col gap-4">
+          <label htmlFor="user_name" className="text-sm font-medium">
+            Email
+          </label>
+          <input
+            type="email"
+            id="user_name"
+            name="user_name"
+            placeholder="Enter your email"
+            className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 text-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500"
+            onChange={handleChange}
+            value={formData.user_name}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          <label htmlFor="password" className="text-sm font-medium">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 text-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500"
+            onChange={handleChange}
+            value={formData.password}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          <label htmlFor="confirm_password" className="text-sm font-medium">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirm_password"
+            name="confirm_password"
+            placeholder="Confirm password"
+            className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 text-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500"
+            onChange={handleChange}
+            value={formData.confirm_password}
+            required
+          />
+        </div>
         {error && <p className="text-red-500">Passwords do not match</p>}
 
         <div className="flex items-center gap-4">
