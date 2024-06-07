00da481c8e35cb24c16086caff13a765b56053ae
Subin <109493447+Subin43@users.noreply.github.com>
2024-06-07 19:58:44 +0530

    Create index.jsx

---
+++ yoga-website-main/src/pages/About/index.jsx
@@ -0,0 +1,9 @@
+import Winner from "./components/Winner";
+
+export default function About() {
+  return (
+    <div className="container mx-auto px-4 md:px-0">
+      <Winner />
+    </div>
+  );
+}
