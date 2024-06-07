e6503ea5ddf6e730c63d807cc254115cb229f7f6
Subin <109493447+Subin43@users.noreply.github.com>
2024-06-07 20:01:53 +0530

    Delete yoga-website-main/src/pages/About/index.tsx

--- yoga-website-main/src/pages/About/index.tsx
+++
@@ -1,9 +0,0 @@
-import Winner from "./components/Winner";
-
-export default function About() {
-  return (
-    <div className="container mx-auto px-4 md:px-0">
-      <Winner />
-    </div>
-  );
-}
